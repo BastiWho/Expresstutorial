30f0ec9dd07b17f6a8bbcec8b621bd9b83b4631f
Basti <sebastian.hufeld@docc.techstarter.de>
2023-07-10 17:24:17 +0200

    Vollständiger Controllers Folder zum Express Tutorial

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,42 +1,159 @@
+const { body, validationResult } = require("express-validator");
 const BookInstance = require("../models/bookinstance");
 const asyncHandler = require("express-async-handler");
+const Book = require("../models/book");
 
-// Anzeige der Liste aller Buchinstanzen.
+
+// Anzeige der Liste aller BookInstances.
 exports.bookinstance_list = asyncHandler(async (req, res, next) => {
-  res.send("NICHT IMPLEMENTIERT: Liste der Buchinstanzen");
+  const allBookInstances = await BookInstance.find().populate("book").exec();
+
+  res.render("bookinstance_list", {
+    title: "Liste der Buchexemplare",
+    bookinstance_list: allBookInstances,
+  });
 });
 
-// Detailseite für eine bestimmte Buchinstanz anzeigen.
+// Detailseite für ein spezifisches Buchexemplar anzeigen.
 exports.bookinstance_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NICHT IMPLEMENTIERT: Detail der Buchinstanz: ${req.params.id}`);
+  const bookInstance = await BookInstance.findById(req.params.id)
+    .populate("book")
+    .exec();
+
+  if (bookInstance === null) {
+    // Keine Ergebnisse.
+    const err = new Error("Buchexemplar nicht gefunden");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("bookinstance_detail", {
+    title: "Buch:",
+    bookinstance: bookInstance,
+  });
 });
 
-// Anzeigen des Buchinstanzen-Erstellungsformulars bei GET.
+// Display BookInstance create form on GET.
 exports.bookinstance_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NICHT IMPLEMENTIERT: Buchinstanz erstellen GET");
+  const allBooks = await Book.find({}, "title").exec();
+
+  res.render("bookinstance_form", {
+    title: "Create BookInstance",
+    book_list: allBooks,
+  });
 });
 
-// Behandlung des Buchinstanzen-Erstellens bei POST.
-exports.bookinstance_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NICHT IMPLEMENTIERT: Buchinstanz erstellen POST");
-});
+// Handle BookInstance create on POST.
+exports.bookinstance_create_post = [
+  // Validate and sanitize fields.
+  body("book", "Book must be specified").trim().isLength({ min: 1 }).escape(),
+  body("imprint", "Imprint must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("status").escape(),
+  body("due_back", "Invalid date")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
 
-// Anzeigen des Buchinstanzen-Löschformulars bei GET.
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a BookInstance object with escaped and trimmed data.
+    const bookInstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors.
+      // Render form again with sanitized values and error messages.
+      const allBooks = await Book.find({}, "title").exec();
+
+      res.render("bookinstance_form", {
+        title: "Create BookInstance",
+        book_list: allBooks,
+        selected_book: bookInstance.book._id,
+        errors: errors.array(),
+        bookinstance: bookInstance,
+      });
+      return;
+    } else {
+      // Data from form is valid
+      await bookInstance.save();
+      res.redirect(bookInstance.url);
+    }
+  }),
+];
+
+// Display book instance delete form on GET.
 exports.bookinstance_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NICHT IMPLEMENTIERT: Buchinstanz löschen GET");
+  const bookInstance = await BookInstance.findById(req.params.id).exec();
+
+  if (bookInstance === null) {
+    // No results.
+    res.redirect("/catalog/bookinstances");
+    return;
+  }
+
+  res.render("bookinstance_delete", {
+    title: "Delete Book Instance",
+    bookinstance: bookInstance,
+  });
 });
 
-// Behandlung des Buchinstanzen-Löschens bei POST.
+// Handle book instance delete on POST.
 exports.bookinstance_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NICHT IMPLEMENTIERT: Buchinstanz löschen POST");
+  const bookInstance = await BookInstance.findById(req.params.id).exec();
+
+  if (bookInstance === null) {
+    // No results.
+    res.redirect("/catalog/bookinstances");
+    return;
+  }
+
+  // Delete the book instance object and redirect to the list of book instances.
+  await BookInstance.findByIdAndRemove(req.body.bookinstanceid);
+  res.redirect("/catalog/bookinstances");
 });
+
 
 // Anzeigen des Buchinstanzen-Aktualisierungsformulars bei GET.
 exports.bookinstance_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NICHT IMPLEMENTIERT: Buchinstanz aktualisieren GET");
+  const bookInstance = await BookInstance.findById(req.params.id).exec();
+
+  if (bookInstance === null) {
+    // No results.
+    res.redirect("/catalog/bookinstances");
+    return;
+  }
+
+  res.render("bookinstance_update", {
+    title: "Update Book Instance",
+    bookinstance: bookInstance,
+  });
 });
 
 // Behandlung der Buchinstanzen-Aktualisierung bei POST.
 exports.bookinstance_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NICHT IMPLEMENTIERT: Buchinstanz aktualisieren POST");
+  const bookInstance = await BookInstance.findById(req.params.id).exec();
+
+  if (bookInstance === null) {
+    // No results.
+    res.redirect("/catalog/bookinstances");
+    return;
+  }
+
+  // Update the book instance object with the new data and redirect to the book instance detail page.
+  bookInstance.status = req.body.status;
+  bookInstance.due_back = req.body.due_back;
+
+  await bookInstance.save();
+
+  res.redirect(bookInstance.url);
 });
--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,42 +1,159 @@
+const { body, validationResult } = require("express-validator");
+const Book = require("../models/book");
 const Genre = require("../models/genre");
 const asyncHandler = require("express-async-handler");
 
-// Display list of all Genre.
+// Display list of all genres.
 exports.genre_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre list");
+  const allGenres = await Genre.find().sort({ name: 1 }).exec();
+  res.render("genre_list", {
+    title: "Genre Liste",
+    genre_list: allGenres,
+  });
 });
 
-// Display detail page for a specific Genre.
+// Detailseite für ein bestimmtes Genre anzeigen.
 exports.genre_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Genre detail: ${req.params.id}`);
+  // Details des Genres und aller zugehörigen Bücher (parallel) abrufen
+  const [genre, booksInGenre] = await Promise.all([
+    Genre.findById(req.params.id).exec(),
+    Book.find({ genre: req.params.id }, "title summary").exec(),
+  ]);
+  if (genre === null) {
+    // Keine Ergebnisse.
+    const err = new Error("Genre nicht gefunden");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("genre_detail", {
+    title: "Genre Detail",
+    genre: genre,
+    genre_books: booksInGenre,
+  });
 });
+
+
 
 // Display Genre create form on GET.
-exports.genre_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre create GET");
-});
+exports.genre_create_get = (req, res, next) => {
+  res.render("genre_form", { title: "Create Genre" });
+};
+
 
 // Handle Genre create on POST.
-exports.genre_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre create POST");
-});
+exports.genre_create_post = [
+  // Validate and sanitize the name field.
+  body("name", "Genre name must contain at least 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
 
-// Display Genre delete form on GET.
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a genre object with escaped and trimmed data.
+    const genre = new Genre({ name: req.body.name });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render("genre_form", {
+        title: "Create Genre",
+        genre: genre,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+      // Check if Genre with same name already exists.
+      const genreExists = await Genre.findOne({ name: req.body.name }).exec();
+      if (genreExists) {
+        // Genre exists, redirect to its detail page.
+        res.redirect(genreExists.url);
+      } else {
+        await genre.save();
+        // New genre saved. Redirect to genre detail page.
+        res.redirect(genre.url);
+      }
+    }
+  }),
+];
+
+
+// Display genre delete form on GET.
 exports.genre_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre delete GET");
+  const genre = await Genre.findById(req.params.id).exec();
+
+  if (genre === null) {
+    // No results.
+    res.redirect("/catalog/genres");
+    return;
+  }
+
+  res.render("genre_delete", {
+    title: "Delete Genre",
+    genre: genre,
+  });
 });
 
-// Handle Genre delete on POST.
+// Handle genre delete on POST.
 exports.genre_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre delete POST");
+  const genre = await Genre.findById(req.params.id).exec();
+
+  if (genre === null) {
+    // No results.
+    res.redirect("/catalog/genres");
+    return;
+  }
+
+  // Genre has associated books. Render in the same way as for GET route.
+  if (genre.books && genre.books.length > 0) {
+    res.render("genre_delete", {
+      title: "Delete Genre",
+      genre: genre,
+    });
+    return;
+  } else {
+    // Genre has no associated books. Delete the genre object and redirect to the list of genres.
+    await Genre.findByIdAndRemove(req.body.genreid);
+    res.redirect("/catalog/genres");
+  }
 });
+
 
 // Display Genre update form on GET.
 exports.genre_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre update GET");
+  const genre = await Genre.findById(req.params.id).exec();
+
+  if (genre === null) {
+    // No results.
+    res.redirect("/catalog/genres");
+    return;
+  }
+
+  res.render("genre_update", {
+    title: "Update Genre",
+    genre: genre,
+  });
 });
 
 // Handle Genre update on POST.
 exports.genre_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre update POST");
+  const genre = await Genre.findById(req.params.id).exec();
+
+  if (genre === null) {
+    // No results.
+    res.redirect("/catalog/genres");
+    return;
+  }
+
+  // Update the genre object with the new data and redirect to the genre detail page.
+  genre.name = req.body.name;
+  genre.description = req.body.description;
+
+  await genre.save();
+
+  res.redirect(genre.url);
 });
