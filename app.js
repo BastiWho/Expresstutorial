49c1a898cdf63e33502f817c26ca7e6fa2fd6c84
Basti <sebastian.hufeld@docc.techstarter.de>
2023-07-10 17:27:01 +0200

    App.js File zum Expresstutorial

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const mongoose = requie("mongoose");
+const mongoose = require("mongoose");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog");
 
 const app = express();
 
 mongoose.set("strictQuery", false);
 
-const mongoDB = "mongodb+srv://<USER>:<password>@cluster0.vhnaqli.mongodb.net/local_library?retryWrites=true&w=majority";
+const mongoDB = "mongodb+srv://<user>:<password>@cluster0.vhnaqli.mongodb.net/local_library?retryWrites=true&w=majority";
 
 main().catch((err) => console.log(err));
 async function main() {
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
